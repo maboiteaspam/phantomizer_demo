151161688f037ffe00f059bdd31a675cdf3cb209
clement <maboiteaspam@wanadoo.fr>
2014-01-31 15:32:29 +0800

    Improved tests

--- test/tests.js
+++ test/tests.js
@@ -13,33 +13,53 @@ describe('phantomizer command line', function () {
     this.timeout(10000);
 
     it('should have a version with the format #.#.#', function(done) {
-        var cmd = base_cmd+" --version ";
-        cp.exec(cmd, function (error, stdout, stderr) {
+        var stdout = "";
+        var stderr = "";
+        var args = [base_cmd,"--version"];
+        var phantomizer = spawn("node", args);
+        phantomizer.stdout.on('data', function (data) {
+            console.log(data.toString());
+            stdout+=data.toString();
+        });
+        phantomizer.stderr.on('data', function (data) {
+            console.log(data.toString());
+            stderr+=data.toString();
+        });
+        phantomizer.on('close', function (code) {
             stdout.should.match(/(phantomizer\s+\d+[.]\d+[.]\d+)/);
             done();
-        })
-            .on("data",function(f){
-                console.log(f)
-            });
+        });
     });
     it('should provide the files in export_dir after export export task', function(done) {
-        var cmd = base_cmd+" --export demo ";
-        cp.exec(cmd, function (error, stdout, stderr) {
+        var stdout = "";
+        var stderr = "";
+        var args = [base_cmd,"--export","demo"];
+        var phantomizer = spawn("node", args);
+        phantomizer.stdout.on('data', function (data) {
+            console.log(data.toString());
+            stdout+=data.toString();
+        });
+        phantomizer.stderr.on('data', function (data) {
+            console.log(data.toString());
+            stderr+=data.toString();
+        });
+        phantomizer.on('close', function (code) {
             (grunt.file.exists(demo_dir+"export/dev/www/index.html")).should.be.true;
             done();
-        })
-            .on("data",function(f){
-                console.log(f)
-            });
+        });
     });
     it('should clean project working directory after clean task', function(done) {
+        var stdout = "";
+        var stderr = "";
         var args = [base_cmd,"--clean","demo"];
         var phantomizer = spawn("node", args);
         phantomizer.stdout.on('data', function (data) {
             console.log(data.toString());
+            stdout+=data.toString();
         });
         phantomizer.stderr.on('data', function (data) {
             console.log(data.toString());
+            stderr+=data.toString();
         });
         phantomizer.on('close', function (code) {
             (grunt.file.exists(demo_dir+"export/dev/www/index.html")).should.be.false;
