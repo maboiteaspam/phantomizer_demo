9999092bfba3381047da31f2b5de2da2f85c549a
clement <maboiteaspam@wanadoo.fr>
2014-02-03 12:05:24 +0800

    Added ie6 support example

---
+++ ie6/project/www-core/js/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+require([
+    "vendors/go-phantomizer/phantomizer"
+],function (phantomizer) {
+
+    phantomizer.render(function(next){
+        $("<span class='test'>It works !</span>").appendTo("body");
+        next();
+    });
+});
