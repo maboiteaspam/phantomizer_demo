bcd186058dcc8d89b0479d3f6ab149c3d116dfe7
clement <maboiteaspam@wanadoo.fr>
2014-02-03 23:11:30 +0800

    Code style

--- demo/project/www-wbm/js/tests/index/index.js
+++ demo/project/www-wbm/js/tests/index/index.js
@@ -8,39 +8,39 @@
 // define a new anonymous module
 define([
 ],function(){
-    // The Test structure
-    // --------------
+  // The Test structure
+  // --------------
 
-    // a set of two methods
-    // init : to load fixtures, patch the rendering
-    // run : actually declare the specs and their assertions
-    function Test(){}
+  // a set of two methods
+  // init : to load fixtures, patch the rendering
+  // run : actually declare the specs and their assertions
+  function Test(){}
 
-    // Fixture loading
-    /**
-     * Load your fixture there
-     * @param next
-     */
-    Test.prototype.init = function(next){
-        next();
-    };
+  // Fixture loading
+  /**
+   * Load your fixture there
+   * @param next
+   */
+  Test.prototype.init = function(next){
+    next();
+  };
 
-    // Specs declaration
-    // --------------
+  // Specs declaration
+  // --------------
 
-    /**
-     * Declare the specs and theirs assertions
-     */
-    Test.prototype.run = function(){
+  /**
+   * Declare the specs and theirs assertions
+   */
+  Test.prototype.run = function(){
 
-        // declare a new asynchronous test (description, assertion count, handler)
-        asyncTest("It displays 'It works !'", 1, function(assert){
-            // ensure the rendered text is correct
-            assert.equal( $(".test").html(), "It works !", "View display 'It works !'" );
-            // do this call to tell qunit it can continue
-            start();
-        });
-    };
-    // **pass the module**
-    return new Test();
+    // declare a new asynchronous test (description, assertion count, handler)
+    asyncTest("It displays 'It works !'", 1, function(assert){
+      // ensure the rendered text is correct
+      assert.equal( $(".test").html(), "It works !", "View display 'It works !'" );
+      // do this call to tell qunit it can continue
+      start();
+    });
+  };
+  // **pass the module**
+  return new Test();
 });
