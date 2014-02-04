b13588b29aa1bcd382f8abfc817bddd33ae4705a
clement <maboiteaspam@wanadoo.fr>
2014-02-04 14:59:59 +0800

    Improved jquery mobile demo

--- jquerymobile/project/www-core/js/index.js
+++ jquerymobile/project/www-core/js/index.js
@@ -5,9 +5,6 @@ require([
 ],function (phantomizer) {
 
   phantomizer.render(function(next){
-    $(document).ready(function(){console.log("ready")})
-    console.log("before ready ???")
-    console.log("ready???")
     $("<span class='test'>It works !</span>").appendTo("body");
     next();
   });
