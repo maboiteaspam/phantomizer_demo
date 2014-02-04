26a85df7090deba70e67c8d12766120c5b16f0d2
clement <maboiteaspam@wanadoo.fr>
2014-02-04 15:15:25 +0800

    Improved jquery mobile demo

--- jquerymobile/project/www-core/js/index.js
+++ jquerymobile/project/www-core/js/index.js
@@ -4,7 +4,14 @@ require([
   "vendors/go-phantomizer/phantomizer",
 ],function (phantomizer) {
 
+  phantomizer.beforeRender(function(next){
+    $("head base").remove();
+    $('<base href="'+window.location.pathname+'" />').prependTo("head"); // required for jqmobile
+    next();
+  },"static");
+
   phantomizer.render(function(next){
+    $.holdReady( false ); // required for jqmobile
     $("<span class='test'>It works !</span>").appendTo("body");
     next();
   });
